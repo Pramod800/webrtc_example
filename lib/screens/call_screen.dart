745cb35d50ba47204612f900705822cddf46794a
Pramod800 <timilsinapramod8888@gmail.com>
2025-05-20 16:39:47 +0545

    ui fixes call screen

--- lib/screens/call_screen.dart
+++ lib/screens/call_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_webrtc/flutter_webrtc.dart';
-import 'package:flutter_webrtc/flutter_webrtc.dart';
 import 'package:mqtt_webrtc_example/services/signaling.dart';
 
 class VideoCallScreen extends StatefulWidget {
@@ -8,16 +7,18 @@ class VideoCallScreen extends StatefulWidget {
   const VideoCallScreen({super.key, required this.roomId});
 
   @override
-  _VideoCallScreenState createState() => _VideoCallScreenState();
+  State<VideoCallScreen> createState() => _VideoCallScreenState();
 }
 
 class _VideoCallScreenState extends State<VideoCallScreen> {
   final RTCVideoRenderer _localRenderer = RTCVideoRenderer();
   final RTCVideoRenderer _remoteRenderer = RTCVideoRenderer();
   late Signaling _signaling;
-  bool _isMicMuted = false;
-  bool _isCameraOff = false;
+  bool _isMicMuted = true;
+  bool isCameraOff = false;
   bool _isLocalVideoOff = false;
+  bool _isRemoteStreamConnected = false;
+  Offset _remoteRendererPosition = const Offset(0, 0);
 
   @override
   void initState() {
@@ -42,6 +43,7 @@ class _VideoCallScreenState extends State<VideoCallScreen> {
     _signaling.onRemoteStream = (stream) {
       setState(() {
         _remoteRenderer.srcObject = stream;
+        _isRemoteStreamConnected = true;
       });
     };
   }
@@ -76,57 +78,87 @@ class _VideoCallScreenState extends State<VideoCallScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Video Call')),
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        title: const Text('Video Call'),
+        leading: IconButton(onPressed: () => Navigator.pop(context), icon: const Icon(Icons.arrow_back_ios_new)),
+      ),
       body: SafeArea(
         child: Stack(
           children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.height,
-              child: RTCVideoView(_localRenderer, mirror: true),
-            ),
-            if (_remoteRenderer.srcObject != null)
-              Positioned(
-                bottom: 20,
-                right: 20,
-                child: Container(
-                  height: 180,
-                  width: 120,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.blueAccent),
-                    borderRadius: BorderRadius.circular(8),
+            _isLocalVideoOff
+                ? Container(
+                    color: Colors.black,
+                    height: double.infinity,
+                    width: double.infinity,
+                    child: const Center(
+                      child: Text(
+                        "Video is off",
+                        style: TextStyle(color: Colors.white, fontSize: 14),
+                      ),
+                    ),
+                  )
+                : SizedBox(
+                    height: double.infinity,
+                    width: double.infinity,
+                    child: RTCVideoView(_localRenderer, mirror: true),
                   ),
-                  child: RTCVideoView(_remoteRenderer),
-                ),
+            Positioned(
+              left: _remoteRendererPosition.dx,
+              top: _remoteRendererPosition.dy,
+              child: Draggable(
+                feedback: RemoteView(remoteRenderer: _remoteRenderer),
+                childWhenDragging: const SizedBox.shrink(),
+                onDragEnd: (details) {
+                  setState(() {
+                    _remoteRendererPosition = details.offset;
+                  });
+                },
+                child: RemoteView(remoteRenderer: _remoteRenderer),
               ),
+            ),
+            Positioned(
+                top: 20,
+                right: 20,
+                child: Text(
+                  _isRemoteStreamConnected ? 'Connected' : 'Connecting....',
+                  style: TextStyle(color: _isLocalVideoOff ? Colors.white : Colors.black),
+                )),
             Positioned(
               bottom: 20,
               left: 20,
               right: 20,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  IconButton(
-                    icon: Icon(
-                      _isMicMuted ? Icons.mic_off : Icons.mic,
-                      color: Colors.blue,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.yellowAccent,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    IconButton(
+                      icon: Icon(
+                        _isMicMuted ? Icons.mic_off : Icons.mic,
+                        color: Colors.blue,
+                      ),
+                      onPressed: _toggleMic,
                     ),
-                    onPressed: _toggleMic,
-                  ),
-                  IconButton(
-                    icon: const Icon(
-                      Icons.switch_camera,
-                      color: Colors.blue,
+                    IconButton(
+                      icon: const Icon(
+                        Icons.switch_camera,
+                        color: Colors.blue,
+                      ),
+                      onPressed: _toggleCamera,
                     ),
-                    onPressed: _toggleCamera,
-                  ),
-                  IconButton(
-                    icon: Icon(
-                      _isLocalVideoOff ? Icons.videocam_off : Icons.videocam,
-                      color: Colors.blue,
+                    IconButton(
+                      icon: Icon(
+                        _isLocalVideoOff ? Icons.videocam_off : Icons.videocam,
+                        color: Colors.blue,
+                      ),
+                      onPressed: _toggleLocalVideo,
                     ),
-                    onPressed: _toggleLocalVideo,
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ],
@@ -143,5 +175,54 @@ class _VideoCallScreenState extends State<VideoCallScreen> {
     _remoteRenderer.dispose();
     _signaling.dispose();
     super.dispose();
+  }
+}
+
+class RemoteView extends StatelessWidget {
+  const RemoteView({
+    super.key,
+    required RTCVideoRenderer remoteRenderer,
+  }) : _remoteRenderer = remoteRenderer;
+
+  final RTCVideoRenderer _remoteRenderer;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 180,
+      width: 120,
+      decoration: BoxDecoration(
+        gradient: const LinearGradient(
+          colors: [Colors.yellowAccent, Colors.greenAccent],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        border: Border.all(color: Colors.blueAccent),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: RTCVideoView(
+        _remoteRenderer,
+        objectFit: RTCVideoViewObjectFit.RTCVideoViewObjectFitCover,
+        placeholderBuilder: (context) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
